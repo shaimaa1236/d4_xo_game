8c5d4e5c0733695c9788c57785f07eee356ec6d0
shaimaa <sh1611111144@outlook.com>
2020-05-08 21:31:59 +0300

    Initial Commit

---
+++ d4_xo_game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  d4_xo_game
+//
+//  Created by Shaimaa on 5/8/20.
+//  Copyright © 2020 Shaimaa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
